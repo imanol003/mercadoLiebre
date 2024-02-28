91fd1830622c1ee75d79b85417ecd81b1d30ef3f
imanol003 <imanolaparicio24@gmail.com>
2024-02-27 21:52:25 -0300

    cambio

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3003;
+
 const path = require("path");
 
 app.use(express.static("public"));
 
+
+const port = process.env.PORT || 3030;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   });
